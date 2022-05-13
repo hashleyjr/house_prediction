c877d491d6c03088d2a56b383298e58a6ca9e3af
hashleyjr <hashleyjr@gmail.com>
2022-05-13 15:11:49 +0200

    prédiction du modèle (premier essai)

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv 
+import numpy as np
+
+import pickle
+
+model=pickle.load(open('model.pkl','rb'))
 
 app = Flask(__name__)
+
+
 
 
 @app.route('/')
@@ -19,6 +26,34 @@ def index():
 @app.route('/contact.html')
 def contact():
 	return render_template("contact.html")
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    # get values of the form
+    city = request.values.get("city")
+    basement = request.values.get("choice_basement")
+    renovated = request.values.get("choice_renovated")
+    floors = request.values.get("floors")
+    waterfront = request.values.get("choice_waterfront")
+    view = request.values.get("view")
+    condition = request.values.get("condition")
+    bedrooms  = request.values.get("bedrooms")
+    bathrooms  = request.values.get("bathrooms")
+    living  = request.values.get("living")
+    lot  = request.values.get("lot")
+    above = request.values.get("above")
+    
+    inputs = np.array([[bedrooms,bathrooms,living,lot,floors,waterfront,view,condition,above,basement,renovated,city]])
+
+    prediction = model.predict(inputs)
+    
+    output='{0:.{1}f}'.format(prediction[0], 0)
+
+    # print prediction
+    return render_template('estimation.html', 
+                           city='{}$'.format(output))
+
 
 def write_to_file(data):
 	with open('database.txt', mode='a') as database:
@@ -46,3 +81,6 @@ def submit_form():
 		
 	else:
 		return "Something went wrong, try again"
+    
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
